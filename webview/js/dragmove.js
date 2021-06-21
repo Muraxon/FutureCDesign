94df297ee6e675c827b2cdfa82a661237be62526
Muraxon <marcel.sotiropoulos@soti-it.at>
2021-06-22 00:46:51 +0200

    little changes for multiselect on drag and drop

--- webview/js/dragmove.js
+++ webview/js/dragmove.js
@@ -9,93 +9,129 @@ const _isTouch = window.ontouchstart !== undefined;
 export const dragmove = function(target, handler, onStart, onEnd) {
   // Register a global event to capture mouse moves (once).
   if (!_loaded) {
-    document.addEventListener(_isTouch ? "touchmove" : "mousemove", function(e) {
-      let c = e;
-      if (e.touches) {
-        c = e.touches[0];
-      }
+	document.addEventListener(_isTouch ? "touchmove" : "mousemove", function(e) {
+	  let c = e;
+	  if (e.touches) {
+		c = e.touches[0];
+	  }
 
-      // On mouse move, dispatch the coords to all registered callbacks.
-      for (var i = 0; i < _callbacks.length; i++) {
-        let restX = c.clientX - (c.clientX % 5);
-        let restY = c.clientY - (c.clientY % 4);
-              
-        _callbacks[i](restX, restY);
-      }
-    });
+	  // On mouse move, dispatch the coords to all registered callbacks.
+	  for (var i = 0; i < _callbacks.length; i++) {
+		let restX = c.clientX - (c.clientX % 5);
+		let restY = c.clientY - (c.clientY % 4);
+			  
+		_callbacks[i](restX, restY, c);
+	  }
+	});
   }
   
   _loaded = true;
   let isMoving = false, hasStarted = false;
   let startX = 0, startY = 0, lastX = 0, lastY = 0;
-  
+  let real_start_x = 0;
+  let real_start_y = 0;
+
   // On the first click and hold, record the offset of the pointer in relation
   // to the point of click inside the element.
   handler.addEventListener(_isTouch ? "touchstart" : "mousedown", function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    if (target.dataset.dragEnabled === "false") {
-      return;
-    }
+	e.stopPropagation();
+	e.preventDefault();
+	if (target.dataset.dragEnabled === "false") {
+	  return;
+	}
 
-    let c = e;
-    if (e.touches) {
-      c = e.touches[0];
-    }
+	let c = e;
+	if (e.touches) {
+	  c = e.touches[0];
+	}
 
-    isMoving = true;
-    startX = target.offsetLeft - c.clientX - (c.clientX % 5);
-    startY = target.offsetTop - c.clientY - (c.clientY % 4);
+	real_start_x = c.clientX;
+	real_start_y = c.clientY;
+
+	isMoving = true;
+	startX = target.offsetLeft - c.clientX;
+	startY = target.offsetTop - c.clientY;
   });
 
   // On leaving click, stop moving.
   document.addEventListener(_isTouch ? "touchend" : "mouseup", function(e) {   
-    if (onEnd && hasStarted) {
-      onEnd(target, parseInt(target.style.left), parseInt(target.style.top));
-    }
+	if (onEnd && hasStarted) {
+	  onEnd(target, parseInt(target.style.left), parseInt(target.style.top));
+	}
 
-    isMoving = false;
-    hasStarted = false;
+	isMoving = false;
+	hasStarted = false;
   });
 
   // Register mouse-move callback to move the element.
-  _callbacks.push(function move(x, y) {
-    if (!isMoving) {
-      return;
-    }
+  _callbacks.push(function move(x, y, c) {
+	if (!isMoving) {
+	  return;
+	}
 
-    if (!hasStarted) {
-      hasStarted = true;
-      if (onStart) {
-        onStart(target, lastX, lastY);
-      }
-    }
+	if (!hasStarted) {
+	  hasStarted = true;
+	  if (onStart) {
+		onStart(target, lastX, lastY);
+	  }
+	}
 
-    lastX = x + startX;
-    lastY = y + startY;
-    
-    lastX -= (lastX % 5);
-    lastY -= (lastY % 4);
+	lastX = x + startX;
+	lastY = y + startY;
+	
+	// lastX -= (lastX % 5);
+	// lastY -= (lastY % 4);
 
-    // If boundary checking is on, don't let the element cross the viewport.
-    if (target.dataset.dragBoundary === "true") {
-      if (lastX < 1 || lastX >= window.innerWidth - target.offsetWidth) {
-        return;
-      }
-      if (lastY < 1 || lastY >= window.innerHeight - target.offsetHeight) {
-        return;
-      }
-    }
+	// If boundary checking is on, don't let the element cross the viewport.
+	if (target.dataset.dragBoundary === "true") {
+	  if (lastX < 1 || lastX >= window.innerWidth - target.offsetWidth) {
+		return;
+	  }
+	  if (lastY < 1 || lastY >= window.innerHeight - target.offsetHeight) {
+		return;
+	  }
+	}
 
-    document.querySelectorAll(".marked_for_group_drag").forEach((el) => {
-      if(el != target) {
-       
-       }
-    });
+	let elements = document.querySelectorAll(".marked_for_group_drag");
 
+	for(let x = 0; x < elements.length; x++) {
+		if(elements[x] != target) {
+			let xClient = c.clientX - (c.clientX % 5);
+			let yClient = c.clientY - (c.clientY % 4);
 
-    target.style.left = lastX + "px";
-    target.style.top = lastY + "px";
+			let real_start_x_temp = xClient - real_start_x;
+			let real_start_y_temp = yClient - real_start_y;
+			
+			real_start_x_temp = (parseInt(elements[x].style.left) + real_start_x_temp);
+			// if(real_start_x_temp % 5 < 3) {
+			// 	real_start_x_temp = real_start_x_temp - (real_start_x_temp % 5);
+			// } else {
+			// 	real_start_x_temp = real_start_x_temp + (5 - (real_start_x_temp % 5));
+			// }
+
+			real_start_y_temp = (parseInt(elements[x].style.top) + real_start_y_temp);
+			// if(real_start_y_temp % 4 < 2) {
+			// 	real_start_y_temp = real_start_y_temp - (real_start_y_temp % 4);
+			// } else {
+			// 	real_start_y_temp = real_start_y_temp + (4 - (real_start_y_temp % 4));
+			// }
+
+			console.log(real_start_y_temp, real_start_x_temp);
+			elements[x].style.left = "" + real_start_x_temp + "px";
+			elements[x].style.top = "" + real_start_y_temp + "px";
+
+			if(elements[x].className.indexOf(" notsaved") < 0) {
+				elements[x].className += " notsaved";
+			}
+		}
+	}
+
+		
+	real_start_x = c.clientX - (c.clientX % 5);
+	real_start_y = c.clientY - (c.clientY % 4);
+
+	target.style.left = lastX + "px";
+	target.style.top = lastY + "px";
   });
 }
 
